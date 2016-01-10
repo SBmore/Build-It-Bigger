aada7e4842e8373adf650a1abcb4a8c1ad9b67aa
SBmore <s.blakemore87@gmail.com>
2016-01-10 22:19:02 +0000

    Feat: Added support for Right to Left layout Feat: Changed target sdk to 15 Feat: After running tests in multiple versions of android there have been changes to the layout for versions pre-Lollipop as the button would not show. There are still issues with the free pre-API21 version though

--- jokedisplay/src/main/java/app/com/udacity/gradle/jokedisplay/JokeActivity.java
+++ jokedisplay/src/main/java/app/com/udacity/gradle/jokedisplay/JokeActivity.java
@@ -1,9 +1,8 @@
 package app.com.udacity.gradle.jokedisplay;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
-import android.view.MenuItem;
 
 public class JokeActivity extends AppCompatActivity {
 
@@ -23,18 +22,18 @@ public class JokeActivity extends AppCompatActivity {
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        int id = item.getItemId();
+//
+//        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+//
+//        return super.onOptionsItemSelected(item);
+//    }
 }
